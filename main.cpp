252442ab44079a7b4dc1e671d9721e2ba1e9ba54
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-05 21:04:39 +0800

    First Commit

--- main.cpp
+++ main.cpp
@@ -8,5 +8,24 @@ struct Node {
 };
 
 int main() {
+    //In every linked list must have a start
+    Node n1, n2, n3, n4, *start;
+    int *ptr;
+
+
+    n1.value = 111;
+    n2.value = 222;
+    n3.value = 333;
+    n4.value = 444;
+
+    start = &n1;
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = NULL;
+
+    ptr = &start->value;
+    cout << *ptr << endl;
+    cout << *ptr << endl;
 
 }
