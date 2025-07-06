740c9d00d4c0846d2431929a5cb22736b8222a26
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-07 00:20:07 +0800

    Studying...:.
    building the app

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <malloc.h>
 
 using namespace std;
 
@@ -8,41 +9,84 @@ struct Node {
     Node *next;
 };
 
-int main() {
-    Node *start, *ptr1, *ptr2;
-    int i, numOfNodes;
-
-    start = NULL;
-    cout << "Enter number of nodes: ";
-    cin >> numOfNodes;
-
-    for (i = 1; i <= numOfNodes; i++) {
-        ptr1 = (Node *)malloc(sizeof(Node));
-        cout << "Enter the value for Node " << i << ": ";
-        cin >> ptr1->value;
-        ptr1->next = NULL;
-
-        if (start == NULL) {
-            start = ptr1;
-            ptr2 = ptr1;
-        }
-        else {
-            ptr2->next = ptr1;
-            ptr2 = ptr2 -> next;
-        }
+bool isListEmpty(Node *start) {
+    if (start == NULL) {
+        return true;
     }
+    return false;
+}
 
-    ptr1 = start;
-    if (ptr1 == NULL) {
+void printItems(Node *p) {
+    int ctr;
+
+    if (isListEmpty(p)) {
         cout << "The list is empty" << endl;
     }
     else {
-        i = 1;
-        while (ptr1 != NULL) {
-            cout << ptr1->value << endl;
-            ++i;
-            ptr1 = ptr1 -> next;
+        ctr = 1;
+        while (p != NULL) {
+            cout << "The value of node " << ctr << " is " << p->value << endl;
+            p = p->next;
+            ++ctr;
         }
     }
+}
+
+void deleteNode(Node *p) {
+
+}
+
+//At the rear
+void insertNodeRear(Node **p, int data) {
+    Node *ptr1, *ptr2;
+    ptr1 = (Node *)malloc(sizeof(Node));
+    ptr1->value = data;
+    ptr1->next = NULL;
+    if (*p == NULL) {
+        *p = ptr1;
+    }
+    else {
+        ptr2 = *p;
+        while (ptr2->next != NULL) {
+            ptr2 = ptr2->next;
+        }
+        ptr2->next = ptr1;
+    }
+}
+//Within linked list
+void insertNodeWithin(Node **p_start, int data, int pos) {
+    Node *ptr, *before, *here;
+    int ctr;
+
+    ptr = (Node *)malloc(sizeof(Node));
+    ptr->value = data;
+    ptr->next = NULL;
+
+    if (pos == 1) {
+        ptr->next = *p_start;
+        *p_start = ptr;
+    }
+    else {
+        before = *p_start;
+        here = (*p_start)->next;
+        ctr = 1;
+        //Divider
+        while (ctr < pos - 2) {
+            before = before->next;
+            here = here->next;
+            ++ctr;
+        }
+        //Linking between
+        before->next = ptr;
+        ptr->next = here;
+    }
+    cout << "\nInsertion at position " << pos << endl;
+    cout << "Item inserted!" << endl;
+}
+
+int main() {
+
+    Node *start;
+    //list should be in the main function
 
 }
